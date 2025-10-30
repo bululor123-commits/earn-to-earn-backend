157e4b22f48c674859d2996d162705c7ef7f71c1
bululor123-commits <bululor123@gmail.com>
2025-10-30 21:04:38 +0700

    Add models/Transaction.js

---
+++ models/models/Transaction.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const transactionSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  type: { type: String }, // 'earn', 'withdraw', 'referral', 'daily'
+  amount: { type: Number },
+  date: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Transaction', transactionSchema);
